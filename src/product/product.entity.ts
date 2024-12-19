598505e63154967c5968840d6ddcbb05aaf035fe
Davyd kennyd sales da silva <kennyd@debian.ifrn.local>
2024-12-19 14:02:05 -0300

    ADICIONEI O BD

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -3,14 +3,14 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number; //ID DO PRODUTO 
 
   @Column()
-  name: string;
+  name: string; //ISSO DAQUI É O NOME DO PRODUTO
 
   @Column()
-  quantity: number;
+  quantity: number; //ISSO DAQUI É A QUANTIDADE
 
   @Column({ default: 5 })
-  lowStockThreshold: number; // Nível de estoque baixo
+  lowStockThreshold: number; //ISSO DAQUI É O NIVEL DE ESTOQUE
 }
